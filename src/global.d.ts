8313f8db366778eedf31ee809d802afd60e755dc
chorisonobeer <152948858+chorisonobeer@users.noreply.github.com>
2024-10-09 23:38:35 +0900

    Update global.d.ts
    画像2から5、TEL追加

--- src/global.d.ts
+++ src/global.d.ts
@@ -9,6 +9,11 @@ declare namespace Pwamap {
     'カテゴリ': string;
     '紹介文': string;
     '画像': string;
+    '画像2': string;  
+    '画像3': string;  
+    '画像4': string;  
+    '画像5': string;  
+    'TEL': string;    
     'URL': string;
     'Instagram': string;
     'Twitter': string;
